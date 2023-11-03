2ae9e2d813410dfee2129f3cc7b41838f0c56142
Castro2023 <charlesugwoke93@gmail.com>
2023-11-03 22:58:48 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const jokeCont =document.getElementById('joke');
+const jokeBtn=document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+async function generateJoke(){
+     
+    const config={headers:{Accept:'application/json'}}
+
+    const response= await fetch('https://icanhazdadjoke.com', config);
+
+    const data= await response.json()
+
+    jokeCont.innerHTML=data.joke
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
